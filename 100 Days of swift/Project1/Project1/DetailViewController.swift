7f6e3afcbba70c66ed978cf9adbdf8e634c9e3e1
Manuel Teixeira <teixeirasmanuel@gmail.com>
2020-04-22 17:03:34 +0100

    Day 17 - Project 1, part two

---
+++ 100 Days of swift/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Manuel Teixeira on 22/04/2020.
+//  Copyright © 2020 Manuel Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+}
