f608bc0ad3c61e70a7685d726b2dadc2817878a0
Manuel Teixeira <teixeirasmanuel@gmail.com>
2020-05-08 09:31:14 +0100

    Day 34 - Project 7, part one

---
+++ 100 Days of swift/Project7/Project7/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Manuel Teixeira on 08/05/2020.
+//  Copyright © 2020 Manuel Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            }
+        }
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = petitions[indexPath.row].title
+        cell.detailTextLabel?.text = petitions[indexPath.row].body
+        
+        return cell
+    }
+
+}
+
